5996cc8216d82594ec77cfc3a33aa01f2b73d83b
Max Fischer <42ducksable1@gmail.com>
2018-08-21 18:53:48 -0500

    IVE COMMITED THIS A LOT JUST PUSH

---
+++ RTS/Assets/Scripts/StatKeepers/colliderScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderScroll : boxSelectUnits
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameObject[] hold = par.unitArray;
+            par.unitArray = new GameObject[unitArray.Length + 1];
+            for (int x = 0; x < hold.Length; x++)
+            {
+                par.unitArray[x] = hold[x];
+            }
+            par.unitArray[unitArray.Length] = other;
+        }
+    }
+
+
+
+}
---
+++ RTS/Assets/Scripts/Units/boxSelectUnits.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class boxSelectUnits : MonoBehaviour {
+
+    public bool enable = true;
+    public GameObject[] unitArray;
+    public GameObject cubeScroll;
+    private GameObject instantiatedCube = null;
+    public GameObject boxObj;
+    public bool isScrolling = false;
+    public boxSelectUnits par;
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!enable)
+            return;
+        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (Input.GetAxisRaw("Fire2") > 0 && isScrolling != true)
+        {
+            instantiatedCube = (GameObject)Instantiate(cubeScroll, Vector3.zero, cubeScroll.transform.rotation);
+            instantiatedCube.GetComponent<RectTransform>().anchoredPosition = mousePos;
+            isScrolling = true;
+        }
+        else if(Input.GetAxisRaw("Fire2") > 0 && isScrolling == true)
+        {
+            instantiatedCube.GetComponent<RectTransform>().anchoredPosition = mousePos;
+        }
+        else if(Input.GetAxisRaw("Fire2") == 0 && isScrolling == true)
+        {
+            GameObject aaa = (GameObject)Instantiate(boxObj, instantiatedCube.transform.position, boxObj.transform.rotation);
+            Destroy(aaa, 1f);
+        }
+        
+
+	}
+
+
+}
