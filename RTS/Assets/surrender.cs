7d5792902ede97510794ab2bd74eb90bf4430a87
maxFischer42 <42ducksable1@gmail.com>
2018-08-28 18:14:38 -0700

    Push any changes lately

--- RTS/Assets/surrender.cs
+++ RTS/Assets/surrender.cs
@@ -7,11 +7,13 @@ public class surrender : MonoBehaviour {
 
     public areasKept area;
     public purchases data;
+    public attackers obj;
 
     private void Start()
     {
         area = GameObject.Find("DataHolderObject").GetComponent<areasKept>();
         data = GameObject.Find("DataHolderObject").GetComponent<purchases>();
+        obj = GameObject.Find("BattleInitiateCanvas").GetComponent<attackers>();
     }
 
 
@@ -21,26 +23,31 @@ public class surrender : MonoBehaviour {
         if (!area.RD)
         {
             area.RD = true;
+            obj.area1 = true;
             SceneManager.LoadScene("Territories");
         }
         else if (!area.MF)
         {
             area.MF = true;
+            obj.area2 = true;
             SceneManager.LoadScene("Territories");
         }
         else if (!area.RP)
         {
             area.RP = true;
+            obj.area3 = true;
             SceneManager.LoadScene("Territories");
         }
         else if (!area.MP)
         {
             area.MP = true;
+            obj.area4 = true;
             SceneManager.LoadScene("Territories");
         }
         else if (!area.RC)
         {
             area.RC = true;
+            obj.area5 = true;
             SceneManager.LoadScene("Territories");
         }
         
